357a8cd5569aae16c78a269ae5dcf7eb329fb1c7
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-11 23:10:53 +0530

    power of four is added

---
+++ src/com/daily_question/Power_of_four_342_06_01_2026.java
@@ -0,0 +1,25 @@
+package com.daily_question;
+
+public class Power_of_four_342_06_01_2026 {
+
+    public static void main(String[] args) {
+
+        int n = 16;
+
+        System.out.println(isPowerOfFour(n));
+
+
+    }
+
+  static  public boolean isPowerOfFour(int n) {
+
+        if( n <= 0){
+            return false;
+        }
+        while(n%4 ==0){
+
+            n = n/4;
+        }
+        return n==1;
+    }
+}
