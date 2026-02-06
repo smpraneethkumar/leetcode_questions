63e1245b0da94013c345dca3f557e36590fcaad9
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-06 21:21:39 +0530

    Minimum_removals_to_balance_Arrays_3634_6_2_26

---
+++ src/com/daily/questions/feb/medium/Minimum_removals_to_balance_Arrays_3634_6_2_26.java
@@ -0,0 +1,31 @@
+package com.daily.questions.feb.medium;
+
+import java.util.Arrays;
+
+class Minimum_removals_to_balance_Arrays_3634_6_2_26 {
+    public int minRemoval(int[] nums, int k) {
+        
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int L = 1;
+
+        long maxEl = nums[0];
+        long minEl = nums[0];
+        int i =0;
+        int j =0;
+
+        while(j<n){
+            minEl = nums[i];
+            maxEl = nums[j];
+
+            while( i< j && maxEl > k*minEl){
+                i++;
+                minEl = nums[i];
+            }   
+             L = Math.max(L,j-i+1);
+             j++;             
+        }
+        return n-L;
+    }
+}
