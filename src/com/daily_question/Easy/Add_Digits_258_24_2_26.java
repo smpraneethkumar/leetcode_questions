33b9d444c34547d2b98f014fc98fde4bd896d95a
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-25 00:07:34 +0530

    now we got the loop is working but the values are coming same and need to to work on it

---
+++ src/com/daily_question/Easy/Add_Digits_258_24_2_26.java
@@ -0,0 +1,27 @@
+package com.daily_question.Easy;
+
+public class Add_Digits_258_24_2_26 {
+
+    public static void main(String[] args) {
+
+        int num = 38;
+String s = String.valueOf(num);
+int ans = 0;
+
+for (int i =0;i<s.length();i++){
+
+    int c = s.charAt(i) ;
+
+    ;
+    ans+= c;
+}
+    System.out.println(ans);
+
+//        System.out.println(s.charAt(0) + s.charAt(1));
+//        System.out.println(addDigits(num));
+    }
+//    public  static int addDigits(int num){
+//
+//        Stri
+//    }
+}
