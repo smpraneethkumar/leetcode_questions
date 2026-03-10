714a46e119dc618d4e0e0bfad5ce75c26bc12d28
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-10 23:55:49 +0530

    Letter_Combinations_of_a_Phone_Number_17 is not done

---
+++ src/com/march/daily_questions/Letter_Combinations_of_a_Phone_Number_17.java
@@ -0,0 +1,34 @@
+package com.march.daily_questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Letter_Combinations_of_a_Phone_Number_17 {
+
+    public static void main(String[] args) {
+
+
+String digits = "23";
+
+        System.out.println(125%100);
+
+//        System.out.println(letterCombinations(digits));
+    }
+//    public static List<String> letterCombinations(String digits) {
+//
+//
+//        if(digits.length() <1){
+//
+//        }
+////        String w2 = "abc";
+//
+//        String [] apl = {" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//
+//        for (int i =0;i<digits.length();i++){
+//
+//            char num = digits.charAt(i);
+//
+//            for(int)
+//        }
+//    }
+}
