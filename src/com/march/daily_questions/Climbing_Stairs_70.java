069ddd8703bb92933def7bbd99917002c3b31f94
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-21 00:24:39 +0530

    no changes

--- src/com/march/daily_questions/Climbing_Stairs_70.java
+++ src/com/march/daily_questions/Climbing_Stairs_70.java
@@ -4,7 +4,7 @@ public class Climbing_Stairs_70 {
 
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(3+));
+        System.out.println(climbStairs(3));
 
     }
 
